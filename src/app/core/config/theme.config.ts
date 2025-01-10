b834e77a4f8bfca6f2a9c91fa6f95d9020eb1133
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2025-01-10 19:03:32 +0100

    add tailwind

--- src/app/core/config/theme.config.ts
+++ src/app/core/config/theme.config.ts
@@ -4,5 +4,10 @@ import { PrimeNGConfigType } from 'primeng/config';
 export const themeConfig: PrimeNGConfigType = {
   theme: {
     preset: AuraBased,
+    options: {
+      cssLayer: {
+        order: 'tailwind-base, primeng, tailwind-utilities',
+      },
+    },
   },
 };
