b20e6e2cc4bf811569663008913d2eec163ab9b1
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2025-01-11 22:08:35 +0100

    add button component

--- src/app/core/directives/index.ts
+++ src/app/core/directives/index.ts
@@ -1,1 +1,2 @@
 export * from './alt-text.directive';
+export * from './long-click.directive';
--- src/app/core/models/shared.model.ts
+++ src/app/core/models/shared.model.ts
@@ -1,3 +1,13 @@
 import { Type } from '@angular/core';
 
 export type Component<T> = Type<T>;
+
+export type ButtonSeverity =
+  | 'success'
+  | 'info'
+  | 'warn'
+  | 'danger'
+  | 'help'
+  | 'primary'
+  | 'secondary'
+  | 'contrast';
