89288a38b0f0b77a5378c1c0a6129bc145872679
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2025-01-12 17:03:20 +0100

    format eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,21 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
     rules: {
-      "no-console": "warn",
-      "@typescript-eslint/no-unused-vars": "error",
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
     },
   },
   {
-    files: ["**/*.js", "**/*.mjs", "**/*.ts"],
+    files: ['**/*.js', '**/*.mjs', '**/*.ts'],
     extends: [eslintPluginPrettierRecommended],
     rules: {
       ...eslintConfigPrettier.rules,
